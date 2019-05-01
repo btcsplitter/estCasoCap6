18d956ddec47c1650d25d8050bbacb00d67ed7f1
Eduardo Penshorn <eduardopenshorn@gmail.com>
2019-05-01 13:22:34 -0300

    Projeto foi criado

---
+++ src/main/java/com/eduardopenshorn/estudoDeCasoCapSeis/EstudoDeCasoCapSeisApplication.java
@@ -0,0 +1,13 @@
+package com.eduardopenshorn.estudoDeCasoCapSeis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDeCasoCapSeisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDeCasoCapSeisApplication.class, args);
+	}
+  
+}
---
+++ src/test/java/com/eduardopenshorn/estudoDeCasoCapSeis/EstudoDeCasoCapSeisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eduardopenshorn.estudoDeCasoCapSeis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstudoDeCasoCapSeisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
